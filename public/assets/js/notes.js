cd3de70035024cbce4318f8306db353a6b9d95f6
Cotes <cotes.brandon@yahoo.com>
2019-02-06 10:16:55 -0500

    front-end page

--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  $("#enter").on("click", function() {
-    const enter = $(this).attr("#enter")
-    $.ajax({
-      url: "/public/notes",
-      method: "GET"
-    }).then(function(data) {
-      location.reload();
-    })
-  })
-});
+// $(document).ready(function() {
+//   $("#enter").on("click", function() {
+//     const enter = $(this).attr("#enter")
+//     $.ajax({
+//       url: "/public/notes",
+//       method: "GET"
+//     }).then(function(data) {
+//       location.reload();
+//     })
+//   })
+// });
