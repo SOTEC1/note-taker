e0b6dfe8ef8ad325dbc4d922911a67fe53145e8c
Cotes <cotes.brandon@yahoo.com>
2019-02-05 23:14:59 -0500

    backend working

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -3,19 +3,31 @@ const db = require("../../db/connection");
 
 router.get("/", function (req, res) {
   // query database for all notes and send back as json
-  
+  db.query("SELECT * FROM notes", function(err, dbRes) {
+    if (err) throw err;
+
+    
+    res.json(dbRes);
+  });
 });
 
 router.post("/", function (req, res) {
   // INSERT into database the data coming from req.body
-});
+  db.query("INSERT INTO notes SET ?", req.body, function(err, result) {
+    if (err) throw err;
 
-router.put("/:id", function (req, res) {
-  // UPDATE database setting req.body WHERE id = req.params.id
+    res.json(result);
+  });
 });
 
 router.delete("/:id", function (req, res) {
   // DELETE from database where id = req.params.id
+  db.query("DELETE FROM notes WHERE id = ?", [req.params.id], 
+  function(err, result) {
+    if (err) throw err;
+
+    res.json(result);
+  })
 });
 
-module.exports = router;
+module.exports = router; 
