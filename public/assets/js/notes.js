e31a569a80f5bc58000c92f73e8439ade4b013f3
Cotes <cotes.brandon@yahoo.com>
2019-02-06 09:14:43 -0500

    front-end

---
+++ public/assets/js/notes.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#enter").on("click", function() {
+    const enter = $(this).attr("#enter")
+    $.ajax({
+      url: "/public/notes",
+      method: "GET"
+    }).then(function(data) {
+      location.reload();
+    })
+  })
+});
